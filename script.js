9b997877d2fcc7902c0ffa257c0281fbd2b06e9d
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-13 20:36:33 +0200

    adding first logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let questions = {
+let questions = [{
     "question_1":"Frage 1",
     "answer_1":"Antwort 1",
     "answer_2":"Antwort 2",
@@ -12,4 +12,17 @@ let questions = {
     "answer_3":"Antwort 3",
     "answer_4":"Antwort 4",
     "answer_":2
+}]
+
+
+let counter = 0;
+
+
+function init(){
+    startQuiz(counter);
+}
+
+
+function startQuiz(counter){
+    document.getElementById("card-title").innerHTML = questions[counter][`question_`+(counter+1)];
 }
