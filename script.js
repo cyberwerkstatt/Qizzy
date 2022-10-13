08352ad42a1df5a8fb5ef554368ad7ae3a095b36
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-13 20:19:19 +0200

    adding first logic

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let questions = {
-    "question":"XXX",
+    "question_1":"Frage 1",
     "answer_1":"Antwort 1",
     "answer_2":"Antwort 2",
     "answer_3":"Antwort 3",
     "answer_4":"Antwort 4",
-    "answer_":3
+    "answer_":3,
 
-    
+    "question_2":"Frage 2",
+    "answer_1":"Antwort 1",
+    "answer_2":"Antwort 2",
+    "answer_3":"Antwort 3",
+    "answer_4":"Antwort 4",
+    "answer_":2
 }
