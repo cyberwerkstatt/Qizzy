6e116a3a7895dbc32b4ea3df99bb5e852d99d5b0
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-13 19:27:23 +0200

    first steps

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = {
+    "question":"XXX",
+    "answer_1":"Antwort 1",
+    "answer_2":"Antwort 2",
+    "answer_3":"Antwort 3",
+    "answer_4":"Antwort 4",
+    "answer_":3
+
+    
+}
