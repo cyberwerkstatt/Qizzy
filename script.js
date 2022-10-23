39451cf14e970a6e088c1b5e878e06300fd192c1
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-24 00:26:56 +0200

    finished Quizzy

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let questions = [
     {
-    "question_1":"Frage 1",
-    "answer_1":"Antwort 1",
-    "answer_2":"Antwort 2",
-    "answer_3":"Antwort 3",
-    "answer_4":"Antwort 4",
-    "right_answer":3
-    },
-
-    {
-    "question_2":"Frage 2",
-    "answer_1":"Antwort 1",
-    "answer_2":"Antwort 2",
-    "answer_3":"Antwort 3",
-    "answer_4":"Antwort 5",
-    "right_answer":2
-    },
-
-    {
-    "question_3":"Frage 3",
-    "answer_1":"Antwort 1",
-    "answer_2":"Antwort 2",
-    "answer_3":"Antwort 3",
-    "answer_4":"Antwort 6",
+    "question_1":"Frage 1: Welchen Beruf übt Doug in der Serie 'King of Queens' aus?",
+    "answer_1":"Lieferwagenfahrer",
+    "answer_2":"Hausmeister",
+    "answer_3":"College-Professor",
+    "answer_4":"Arzt",
     "right_answer":1
+    },
+
+    {
+    "question_2":"Frage 2: Wie heißt Dougs Schwiegervater?",
+    "answer_1":"Thomas",
+    "answer_2":"James",
+    "answer_3":"John",
+    "answer_4":"Arthur",
+    "right_answer":4
+    },
+
+    {
+    "question_3":"Frage 3: In welchem Stadtteil von New York wohnt Doug?",
+    "answer_1":"Staten Island",
+    "answer_2":"Brooklyn",
+    "answer_3":"Queens",
+    "answer_4":"Manhatten",
+    "right_answer":3
     }
 ]
 
@@ -46,37 +46,34 @@ function init(){
 
 function startQuiz(counter){
     let idOfQuestion = `question_${counter+1}`
-    document.getElementById("card-title").innerHTML = questions[counter][idOfQuestion];
-        
+    document.getElementById("card-title").innerHTML = questions[counter][idOfQuestion];    
     for(let i = 1; i <= 4; i++){
         let idOfAnswers = `answer_${i}`
         document.getElementById(idOfAnswers).innerHTML = `<a class="card-body" href="#">${questions[counter][idOfAnswers]}</a>`;
     }
-    
 }
 
 
 function checkAnswer(selection){
-    right_answer = questions[counter]["right_answer"]
-    selected_answer = selection.slice(-1)
-    id_right_answer = `answer_${right_answer}`
+    right_answer = questions[counter]["right_answer"];
+    selected_answer = selection.slice(-1);
+    id_right_answer = `answer_${right_answer}`;
    if (selected_answer == right_answer){
     counter++;
     trueAnswer++;
-    document.getElementById(selection).classList.add("bg-success")
-    checkProgress()
-    disableAnswers()
+    document.getElementById(selection).classList.add("bg-success");
+    checkProgress();
+    disableAnswers();
     audioSucess.play();
    } else {
     counter++;
-    document.getElementById(selection).classList.add("bg-danger")
-    document.getElementById(id_right_answer).classList.add("bg-success")
-    checkProgress()
-    disableAnswers()
+    document.getElementById(selection).classList.add("bg-danger");
+    document.getElementById(id_right_answer).classList.add("bg-success");
+    checkProgress();
+    disableAnswers();
     audioFail.play();
    }
-
-   document.getElementById("button").removeAttribute("disabled","disabled")
+   document.getElementById("button").removeAttribute("disabled","disabled");
 }
 
 function nextQuestion(){    
@@ -87,22 +84,22 @@ function nextQuestion(){
         init();
         document.getElementById(`answer_${questions[counter-1].right_answer}`).classList.remove("bg-success");
         resetButton();
-        enableAnswers()
+        enableAnswers();
     }
 }
 
 function checkProgress(){
-    let progress = Math.round((counter/questions.length)*100)
+    let progress = Math.round((counter/questions.length)*100);
     document.getElementById("progress-bar").style.width = progress+'%';
     document.getElementById("progress-bar").innerHTML = progress+'%';
 }
 
 function resetButton(){
     for(let i = 1; i <=4; i++){
-        document.getElementById(`answer_${i}`).classList.remove("bg-danger")
-        document.getElementById(`answer_${i}`).classList.remove("bg-success")
+        document.getElementById(`answer_${i}`).classList.remove("bg-danger");
+        document.getElementById(`answer_${i}`).classList.remove("bg-success");
     }
-    document.getElementById("button").setAttribute("disabled","disabled")
+    document.getElementById("button").setAttribute("disabled","disabled");
 }
 
 function endScreen(){
@@ -112,12 +109,6 @@ function endScreen(){
     aplaus.play();
     confetti();
 }
-
-confetti({
-    particleCount: 1000,
-    spread: 1000,
-    origin: { y: 3.6 }
-  });
 
 function disableAnswers(){
     for(let i = 1; i <= 4; i++){
@@ -158,10 +149,10 @@ function restartQuiz(){
     document.getElementById("progress-bar").style.width = '0%';
     document.getElementById("progress-bar").innerHTML = '';
     for(let i = 1; i <=4; i++){
-        document.getElementById(`answer_${i}`).classList.remove("bg-danger")
-        document.getElementById(`answer_${i}`).classList.remove("bg-success")
+        document.getElementById(`answer_${i}`).classList.remove("bg-danger");
+        document.getElementById(`answer_${i}`).classList.remove("bg-success");
     }
     init();
     enableAnswers();
-    document.getElementById("button").setAttribute("disabled","enabled")
+    document.getElementById("button").setAttribute("disabled","enabled");
 }
